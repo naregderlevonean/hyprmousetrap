ce5939c8982796c643c4ebf0c42f0f9848a10b4a
Nareg der Levonean <naregderlevonean@gmail.com>
2026-05-10 15:44:05 +0300

    Touchpad support

--- src/input.rs
+++ src/input.rs
@@ -40,28 +40,67 @@ impl MouseManager {
 
                         if !active_devices.contains(&path_str) {
                             if let Ok(mut device) = evdev::Device::open(&path) {
-                                if device.supported_events().contains(evdev::EventType::RELATIVE) {
+                                let has_rel = device.supported_events().contains(evdev::EventType::RELATIVE);
+                                let has_abs = device.supported_events().contains(evdev::EventType::ABSOLUTE);
+
+                                if has_rel || has_abs {
                                     active_devices.insert(path_str.clone());
                                     let dx_c = dx.clone();
                                     let dy_c = dy.clone();
 
-                                    thread::spawn(move || loop {
-                                        match device.fetch_events() {
-                                            Ok(events) => {
-                                                for ev in events {
-                                                    if ev.event_type() == evdev::EventType::RELATIVE {
-                                                        match ev.code() {
-                                                            0x00 => { dx_c.fetch_add(ev.value() as i32, Ordering::Relaxed); }
-                                                            0x01 => { dy_c.fetch_add(ev.value() as i32, Ordering::Relaxed); }
-                                                            _ => {}
+                                    thread::spawn(move || {
+                                        let mut last_abs_x: Option<i32> = None;
+                                        let mut last_abs_y: Option<i32> = None;
+
+                                        loop {
+                                            match device.fetch_events() {
+                                                Ok(events) => {
+                                                    for ev in events {
+                                                        if ev.event_type() == evdev::EventType::RELATIVE {
+                                                            match ev.code() {
+                                                                0x00 => { dx_c.fetch_add(ev.value() as i32, Ordering::Relaxed); }
+                                                                0x01 => { dy_c.fetch_add(ev.value() as i32, Ordering::Relaxed); }
+                                                                _ => {}
+                                                            }
+                                                        } 
+                                                        else if ev.event_type() == evdev::EventType::ABSOLUTE {
+                                                            match ev.code() {
+                                                                0x00 | 0x35 => { 
+                                                                    let val = ev.value() as i32;
+                                                                    if let Some(last) = last_abs_x {
+                                                                        dx_c.fetch_add(val - last, Ordering::Relaxed);
+                                                                    }
+                                                                    last_abs_x = Some(val);
+                                                                }
+                                                                0x01 | 0x36 => {
+                                                                    let val = ev.value() as i32;
+                                                                    if let Some(last) = last_abs_y {
+                                                                        dy_c.fetch_add(val - last, Ordering::Relaxed);
+                                                                    }
+                                                                    last_abs_y = Some(val);
+                                                                }
+                                                                0x39 => { 
+                                                                    if ev.value() == -1 {
+                                                                        last_abs_x = None;
+                                                                        last_abs_y = None;
+                                                                    }
+                                                                }
+                                                                _ => {}
+                                                            }
+                                                        }
+                                                        else if ev.event_type() == evdev::EventType::KEY {
+                                                            if ev.code() == 330 && ev.value() == 0 {
+                                                                last_abs_x = None;
+                                                                last_abs_y = None;
+                                                            }
                                                         }
                                                     }
                                                 }
+                                                Err(e) if e.kind() == std::io::ErrorKind::WouldBlock => {
+                                                    thread::sleep(Duration::from_millis(10));
+                                                }
+                                                Err(_) => break,
                                             }
-                                            Err(e) if e.kind() == std::io::ErrorKind::WouldBlock => {
-                                                thread::sleep(Duration::from_millis(10));
-                                            }
-                                            Err(_) => break,
                                         }
                                     });
                                 }
